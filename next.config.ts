f54145114e8ffe9ea33a0f3f762c3aa9c004c65c
Yega13 <yeganyansuren13@gmail.com>
2026-03-13 18:24:17 +0400

    cloudflare and github sk push

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/*": ["./node_modules/@node-rs/**/*.node"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
